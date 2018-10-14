79e73608219e6b730159cb24084841d949047f3f
Jase Kirby <jasekirby7@gmail.com>
2018-10-14 14:22:42 -0400

    added CI/CD files

---
+++ Makefile
@@ -0,0 +1,24 @@
+# run from git bash only on windows
+APPNAME=smellnet
+VERSION ?= vlatest
+GOBIN=$(shell pwd)/bin
+GOFILES=$(wildcard *.go)
+
+build:
+	GOOS=windows GOARCH=amd64 GOBIN=$(GOBIN) go build -o bin/$(APPNAME).exe
+
+clean:
+	echo "Cleaning"
+	GOBIN=$(GOBIN) go clean
+	rm -rf $(GOBIN)/*
+
+windows:
+	GOOS=windows GOARCH=amd64 GOBIN=$(GOBIN) go build -o bin/release/$(APPNAME)-$(VERSION)-windows-amd64/$(APPNAME).exe
+	zip -r bin/release/$(APPNAME)-$(VERSION)-windows-amd64 bin/release/$(APPNAME)-$(VERSION)-windows-amd64
+linux:
+	GOOS=windows GOARCH=amd64 GOBIN=$(GOBIN) go build -o bin/release/$(APPNAME)-$(VERSION)-linux-amd64/$(APPNAME)
+	tar -zcvf bin/release/$(APPNAME)-$(VERSION)-linux-amd64.tar.gz bin/release/$(APPNAME)-$(VERSION)-linux-amd64
+
+release: windows linux
+
+.PHONY: build clean windows linux release
--- smellnet.go
+++ smellnet.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"net/http"
 	"os"
 	"strconv"
 	"time"
@@ -25,14 +26,23 @@ var timeout int
 const timeoutDef int = 3
 const timeoutDesc string = "how long to wait before timing out and terminating the smell"
 
+var httpReq, httpsReq bool
+
+const httpDef bool = false
+const httpDesc string = "attempt an http GET request"
+const httpsDef bool = false
+const httpsDesc string = "attempt an https GET request"
+
 func main() {
 	fmt.Println("starting smeller")
 	flag.StringVar(&address, "address", addrDef, addrDesc)
 	flag.StringVar(&address, "a", addrDef, addrDesc+" (shorthand)")
 	flag.IntVar(&port, "port", portDef, portDesc)
-	flag.IntVar(&port, "p", portDef, portDesc)
+	flag.IntVar(&port, "p", portDef, portDesc+" (shorthand)")
 	flag.IntVar(&timeout, "timeout", timeoutDef, timeoutDesc)
-	flag.IntVar(&timeout, "t", timeoutDef, timeoutDesc)
+	flag.IntVar(&timeout, "t", timeoutDef, timeoutDesc+" (shorthand)")
+	flag.BoolVar(&httpReq, "http", httpDef, httpDesc)
+	flag.BoolVar(&httpsReq, "https", httpsDef, httpsDesc)
 	flag.Parse()
 
 	ip, err := net.LookupIP(address)
@@ -43,12 +53,18 @@ func main() {
 	fmt.Printf("dns names: %s\n", dnsNames)
 	isAlive, elapsedTimeMessage, err := tcpAlive()
 	if err == nil {
-		fmt.Printf("tcp port alive: %t\n", isAlive)
+		fmt.Printf("tcp port alive:\t %t\n", isAlive)
 		fmt.Println(elapsedTimeMessage)
 	} else {
 		fmt.Println("no response from port")
 		os.Exit(1)
 	}
+	if httpReq {
+		respStatus, respTime := httpGet("http://")
+		fmt.Printf("http status: %s\n", respStatus)
+		fmt.Println(respTime)
+	}
+	// https request
 }
 
 func tcpAlive() (bool, string, error) {
@@ -62,7 +78,6 @@ func tcpAlive() (bool, string, error) {
 			conn = nil
 			return false, elapsedTimeMessage(start), err
 		}
-
 		return true, elapsedTimeMessage(start), nil
 	}
 	return false, elapsedTimeMessage(start), err
@@ -71,4 +86,19 @@ func tcpAlive() (bool, string, error) {
 func elapsedTimeMessage(start time.Time) string {
 	elapsed := time.Since(start)
 	return fmt.Sprintf("took %s", elapsed)
+}
+
+func httpGet(protocol string) (string, string) {
+	start := time.Now()
+	fullAddr := protocol + net.JoinHostPort(address, strconv.Itoa(port))
+	resp, err := http.Get(fullAddr)
+	if err != nil {
+		fmt.Print(err)
+	}
+	defer resp.Body.Close()
+	respTime := elapsedTimeMessage(start)
+	//body, err := ioutil.ReadAll(resp.Body)
+	//fmt.Print(string(body[:]))
+
+	return resp.Status, respTime
 }
